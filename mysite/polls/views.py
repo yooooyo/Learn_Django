5146b794da59c19a24abcbe44c109fccab6e9898
yooooyo <loipswqe@gmail.com>
2020-10-08 01:07:48 +0800

    part3 A shortcut: render()

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,14 +5,21 @@ from django.template import loader
 from .models import Question
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'lastest_question_list':latest_question_list
-    }
-    return HttpResponse(template.render(context,request))
 
+#use loader
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'lastest_question_list':latest_question_list
+#     }
+#     return HttpResponse(template.render(context,request))
+
+#use render (common use)
+def index(request):
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'lastest_question_list':lastest_question_list}
+    return render(request,'polls/index.html',context)
 
 def detail(request,question_id):
     return HttpResponse(f'Looking question at {question_id}')
