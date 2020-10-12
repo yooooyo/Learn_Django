b00508c660dc207fef43ca0964372f814c18abc2
yooooyo <loipswqe@gmail.com>
2020-10-12 11:53:24 +0800

    part 7 :Customize the admin change list

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -6,15 +6,23 @@ from .models import Question,Choice
 
 admin.site.register(Choice)
 
+##############################################
+# use
+# class ChoiceInline(admin.StackedInline):
+#     model = Choice
+#     extra = 3
+# or
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
+#############################################
 
 class QuestionAdmin(admin.ModelAdmin):
     filedsets = [
         (None,  {'fields':['question_text']}),
         ('Date information' ,{'fields':['pub_date']}),
     ]
+    list_display = ('question_text','pub_date','was_published_recently')
     inlines = [ChoiceInline]
     
 
