856bf6591f773db53410a1b2ed56d0c89e4d3ee7
yooooyo <loipswqe@gmail.com>
2020-10-12 11:32:55 +0800

    part 7 :choose admin form field

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from .models import Question,Choice
 # Register your models here.
 
-admin.site.register(Question)
+
 admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date','question_text']
+
+admin.site.register(Question,QuestionAdmin)
